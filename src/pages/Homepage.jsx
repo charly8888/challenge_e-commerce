a547e0faa0221b95793de75b6c18b82962b9d992
German Hornus <hornusgerman@gmail.com>
2022-04-14 19:52:35 -0500

    init

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  Footer,
+  Header,
+  PictureSection,
+  Products,
+  Selectors,
+} from '/src/Components'
+
+export const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <PictureSection />
+      <Selectors />
+      <Products />
+      <Footer />
+    </>
+  )
+}
